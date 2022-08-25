068038de440912a9b30cac6f289d9166b74a14a5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-25 15:07:43 +0200

    Increase benchmark size

--- bench.mjs
+++ bench.mjs
@@ -5,11 +5,11 @@ import { cnt } from './index.js'
 test('cnt', async (t) => {
   const ops = 1000000
 
-  const buf = new Uint32Array(256).fill(0xffffffff)
-  const expected = new Uint32Array(256).fill(32)
+  const buf = new Uint32Array(2048).fill(0xffffffff)
+  const expected = new Uint32Array(2048).fill(32)
 
   await t.test('simd', async (t) => {
-    const result = new Uint32Array(256)
+    const result = new Uint32Array(2048)
 
     const elapsed = await t.execution(() => {
       for (let i = 0; i < ops; i++) {
@@ -29,11 +29,11 @@ test('cnt', async (t) => {
       return ((n + (n >> 4) & 0xf0f0f0f) * 0x1010101) >> 24
     }
 
-    const result = new Uint32Array(256)
+    const result = new Uint32Array(2048)
 
     const elapsed = await t.execution(() => {
       for (let i = 0; i < ops; i++) {
-        for (let j = 0; j < 256; j += 4) {
+        for (let j = 0; j < 2048; j += 4) {
           result[j] = cnt(buf[j])
           result[j + 1] = cnt(buf[j + 1])
           result[j + 2] = cnt(buf[j + 2])
