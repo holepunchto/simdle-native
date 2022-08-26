7e19efbddb040da9e6e57dd6be6c17ceea72f5fa
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-26 14:57:58 +0200

    Bind remaining functions

--- binding.c
+++ binding.c
@@ -2,6 +2,78 @@
 #include <node_api.h>
 #include <simd.h>
 
+NAPI_METHOD(simd_napi_clo_v128_u8) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clo_v128_u8(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_clo_v128_u16) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clo_v128_u16(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_clo_v128_u32) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clo_v128_u32(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_clz_v128_u8) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clz_v128_u8(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_clz_v128_u16) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clz_v128_u16(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_clz_v128_u32) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_clz_v128_u32(buf[i]);
+  }
+
+  return NULL;
+}
+
 NAPI_METHOD(simd_napi_cnt_v128_u8) {
   NAPI_ARGV(2);
   NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
@@ -38,10 +110,136 @@ NAPI_METHOD(simd_napi_cnt_v128_u32) {
   return NULL;
 }
 
+NAPI_METHOD(simd_napi_cto_v128_u8) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_cto_v128_u8(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_cto_v128_u16) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_cto_v128_u16(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_cto_v128_u32) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_cto_v128_u32(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_ctz_v128_u8) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_ctz_v128_u8(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_ctz_v128_u16) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_ctz_v128_u16(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_ctz_v128_u32) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_ctz_v128_u32(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_not_v128_u8) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_not_v128_u8(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_not_v128_u16) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_not_v128_u16(buf[i]);
+  }
+
+  return NULL;
+}
+
+NAPI_METHOD(simd_napi_not_v128_u32) {
+  NAPI_ARGV(2);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, buf, 0);
+  NAPI_ARGV_BUFFER_CAST(simd_v128_t *, result, 1);
+
+  for (size_t i = 0, n = buf_len / 16; i < n; i++) {
+    result[i] = simd_not_v128_u32(buf[i]);
+  }
+
+  return NULL;
+}
+
 NAPI_INIT() {
-  NAPI_EXPORT_SIZEOF(simd_v128_t);
+  NAPI_EXPORT_FUNCTION(simd_napi_clo_v128_u8);
+  NAPI_EXPORT_FUNCTION(simd_napi_clo_v128_u16);
+  NAPI_EXPORT_FUNCTION(simd_napi_clo_v128_u32);
+
+  NAPI_EXPORT_FUNCTION(simd_napi_clz_v128_u8);
+  NAPI_EXPORT_FUNCTION(simd_napi_clz_v128_u16);
+  NAPI_EXPORT_FUNCTION(simd_napi_clz_v128_u32);
 
   NAPI_EXPORT_FUNCTION(simd_napi_cnt_v128_u8);
   NAPI_EXPORT_FUNCTION(simd_napi_cnt_v128_u16);
   NAPI_EXPORT_FUNCTION(simd_napi_cnt_v128_u32);
+
+  NAPI_EXPORT_FUNCTION(simd_napi_cto_v128_u8);
+  NAPI_EXPORT_FUNCTION(simd_napi_cto_v128_u16);
+  NAPI_EXPORT_FUNCTION(simd_napi_cto_v128_u32);
+
+  NAPI_EXPORT_FUNCTION(simd_napi_ctz_v128_u8);
+  NAPI_EXPORT_FUNCTION(simd_napi_ctz_v128_u16);
+  NAPI_EXPORT_FUNCTION(simd_napi_ctz_v128_u32);
+
+  NAPI_EXPORT_FUNCTION(simd_napi_not_v128_u8);
+  NAPI_EXPORT_FUNCTION(simd_napi_not_v128_u16);
+  NAPI_EXPORT_FUNCTION(simd_napi_not_v128_u32);
 }
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const binding = require('node-gyp-build')(__dirname)
 const b4a = require('b4a')
 
+exports.clo = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
+  if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
+  if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
+
+  const n = buf.BYTES_PER_ELEMENT
+
+  if (n === 1) binding.simd_napi_clo_v128_u8(buf, result)
+  else if (n === 2) binding.simd_napi_clo_v128_u16(buf, result)
+  else binding.simd_napi_clo_v128_u32(buf, result)
+
+  return result
+}
+
+exports.clz = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
+  if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
+  if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
+
+  const n = buf.BYTES_PER_ELEMENT
+
+  if (n === 1) binding.simd_napi_clz_v128_u8(buf, result)
+  else if (n === 2) binding.simd_napi_clz_v128_u16(buf, result)
+  else binding.simd_napi_clz_v128_u32(buf, result)
+
+  return result
+}
+
 exports.cnt = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
   if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
   if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
@@ -10,6 +36,45 @@ exports.cnt = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
   if (n === 1) binding.simd_napi_cnt_v128_u8(buf, result)
   else if (n === 2) binding.simd_napi_cnt_v128_u16(buf, result)
   else binding.simd_napi_cnt_v128_u32(buf, result)
+
+  return result
+}
+
+exports.cto = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
+  if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
+  if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
+
+  const n = buf.BYTES_PER_ELEMENT
+
+  if (n === 1) binding.simd_napi_cto_v128_u8(buf, result)
+  else if (n === 2) binding.simd_napi_cto_v128_u16(buf, result)
+  else binding.simd_napi_cto_v128_u32(buf, result)
+
+  return result
+}
+
+exports.ctz = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
+  if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
+  if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
+
+  const n = buf.BYTES_PER_ELEMENT
+
+  if (n === 1) binding.simd_napi_ctz_v128_u8(buf, result)
+  else if (n === 2) binding.simd_napi_ctz_v128_u16(buf, result)
+  else binding.simd_napi_ctz_v128_u32(buf, result)
+
+  return result
+}
+
+exports.not = function (buf, result = b4a.allocUnsafe(buf.byteLength)) {
+  if (buf.byteLength % 16 !== 0) throw new Error('Buffer length must be a multiple of 16')
+  if (buf.byteLength > result.byteLength) throw new Error('Length of result buffer is insufficient')
+
+  const n = buf.BYTES_PER_ELEMENT
+
+  if (n === 1) binding.simd_napi_not_v128_u8(buf, result)
+  else if (n === 2) binding.simd_napi_not_v128_u16(buf, result)
+  else binding.simd_napi_not_v128_u32(buf, result)
 
   return result
 }
