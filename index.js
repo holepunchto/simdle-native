a609a42e7fb12767e7b230f66f97f691fe9373d1
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-07 15:14:25 +0100

    Migrate to `libjstl` (#4)
    Co-authored-by: Tony Ivanov <telamohn@pm.me>

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const binding = require('./binding')
 const b4a = require('b4a')
 
+function predicate(u8, u16, u32) {
+  return function predicate(buf) {
+    if (buf.byteLength % 16 !== 0) {
+      throw new Error('Buffer length must be a multiple of 16')
+    }
+
+    const n = buf.BYTES_PER_ELEMENT
+
+    if (n === 1) return u8(buf)
+    if (n === 2) return u16(buf)
+    return u32(buf)
+  }
+}
+
 function unary(u8, u16, u32) {
   return function unary(buf, result = b4a.allocUnsafe(buf.byteLength)) {
     if (buf.byteLength % 16 !== 0) {
@@ -55,84 +69,80 @@ function reduce(u8, u16, u32) {
   }
 }
 
-exports.allo = function allo(buf) {
-  if (buf.byteLength % 16 !== 0) {
-    throw new Error('Buffer length must be a multiple of 16')
-  }
+exports.allo = predicate(
+  binding.simdle_native_allo_v128_u8,
+  binding.simdle_native_allo_v128_u16,
+  binding.simdle_native_allo_v128_u32
+)
 
-  return binding.simdle_napi_allo_v128(buf) !== 0
-}
-
-exports.allz = function allz(buf) {
-  if (buf.byteLength % 16 !== 0) {
-    throw new Error('Buffer length must be a multiple of 16')
-  }
-
-  return binding.simdle_napi_allz_v128(buf) !== 0
-}
+exports.allz = predicate(
+  binding.simdle_native_allz_v128_u8,
+  binding.simdle_native_allz_v128_u16,
+  binding.simdle_native_allz_v128_u32
+)
 
 exports.and = binary(
-  binding.simdle_napi_and_v128_u8,
-  binding.simdle_napi_and_v128_u16,
-  binding.simdle_napi_and_v128_u32
+  binding.simdle_native_and_v128_u8,
+  binding.simdle_native_and_v128_u16,
+  binding.simdle_native_and_v128_u32
 )
 
 exports.clear = binary(
-  binding.simdle_napi_clear_v128_u8,
-  binding.simdle_napi_clear_v128_u16,
-  binding.simdle_napi_clear_v128_u32
+  binding.simdle_native_clear_v128_u8,
+  binding.simdle_native_clear_v128_u16,
+  binding.simdle_native_clear_v128_u32
 )
 
 exports.clo = unary(
-  binding.simdle_napi_clo_v128_u8,
-  binding.simdle_napi_clo_v128_u16,
-  binding.simdle_napi_clo_v128_u32
+  binding.simdle_native_clo_v128_u8,
+  binding.simdle_native_clo_v128_u16,
+  binding.simdle_native_clo_v128_u32
 )
 
 exports.clz = unary(
-  binding.simdle_napi_clz_v128_u8,
-  binding.simdle_napi_clz_v128_u16,
-  binding.simdle_napi_clz_v128_u32
+  binding.simdle_native_clz_v128_u8,
+  binding.simdle_native_clz_v128_u16,
+  binding.simdle_native_clz_v128_u32
 )
 
 exports.cnt = unary(
-  binding.simdle_napi_cnt_v128_u8,
-  binding.simdle_napi_cnt_v128_u16,
-  binding.simdle_napi_cnt_v128_u32
+  binding.simdle_native_cnt_v128_u8,
+  binding.simdle_native_cnt_v128_u16,
+  binding.simdle_native_cnt_v128_u32
 )
 
 exports.cto = unary(
-  binding.simdle_napi_cto_v128_u8,
-  binding.simdle_napi_cto_v128_u16,
-  binding.simdle_napi_cto_v128_u32
+  binding.simdle_native_cto_v128_u8,
+  binding.simdle_native_cto_v128_u16,
+  binding.simdle_native_cto_v128_u32
 )
 
 exports.ctz = unary(
-  binding.simdle_napi_ctz_v128_u8,
-  binding.simdle_napi_ctz_v128_u16,
-  binding.simdle_napi_ctz_v128_u32
+  binding.simdle_native_ctz_v128_u8,
+  binding.simdle_native_ctz_v128_u16,
+  binding.simdle_native_ctz_v128_u32
 )
 
 exports.not = unary(
-  binding.simdle_napi_not_v128_u8,
-  binding.simdle_napi_not_v128_u16,
-  binding.simdle_napi_not_v128_u32
+  binding.simdle_native_not_v128_u8,
+  binding.simdle_native_not_v128_u16,
+  binding.simdle_native_not_v128_u32
 )
 
 exports.or = binary(
-  binding.simdle_napi_or_v128_u8,
-  binding.simdle_napi_or_v128_u16,
-  binding.simdle_napi_or_v128_u32
+  binding.simdle_native_or_v128_u8,
+  binding.simdle_native_or_v128_u16,
+  binding.simdle_native_or_v128_u32
 )
 
 exports.sum = reduce(
-  binding.simdle_napi_sum_v128_u8,
-  binding.simdle_napi_sum_v128_u16,
-  binding.simdle_napi_sum_v128_u32
+  binding.simdle_native_sum_v128_u8,
+  binding.simdle_native_sum_v128_u16,
+  binding.simdle_native_sum_v128_u32
 )
 
 exports.xor = binary(
-  binding.simdle_napi_xor_v128_u8,
-  binding.simdle_napi_xor_v128_u16,
-  binding.simdle_napi_xor_v128_u32
+  binding.simdle_native_xor_v128_u8,
+  binding.simdle_native_xor_v128_u16,
+  binding.simdle_native_xor_v128_u32
 )
